539b96d0e43030bd9a8e87267d850930c0bd344d
BounAbdallah <bigmabo61345@gmail.com>
2022-03-16 01:01:50 +0000

    .

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const logger = require('morgan');
 const db = require("./models");
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const platsRouter = require('./routes/plats');
 
 db.sequelize.sync();
 
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/plats', platsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
